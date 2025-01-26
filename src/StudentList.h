891393224a8468cdcabec41a3c218a28b96cb2a1
Semyon <zhigunovsemen@gmail.com>
2025-01-26 13:03:50 +0300

    Добавил StudentList.h

---
+++ src/StudentList.h
@@ -0,0 +1,114 @@
+#include "Double.h"
+#include "Int.h"
+#include "String.h"
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <numeric>
+#include <ostream>
+#include <vector>
+
+/*
+ * Необходимо предусмотреть создание заголовка таблицы со столбцами объектов
+ * выбранных типов, добавление, удаление, редактирование строк, сортировку по
+ * любому столбцу, сохранение и загрузку таблицы (структуры и содержимого)
+ * в текстовом файле.
+ */
+
+/*Список студентов*/
+class StudentList {
+public:
+	/*Добавление нового студента по готовым данным*/
+	StudentList & add(Int N,	   /*Номер студента*/
+			  String Surname,  /*Фамилия*/
+			  String Name,	   /*Имя*/
+			  String Patronim, /*Отчество*/
+			  Int Mark1,	   /*Оценка студента*/
+			  Int Mark2,	   /*Оценка студента*/
+			  Int Mark3,	   /*Оценка студента*/
+			  Int Mark4,	   /*Оценка студента*/
+			  Int Mark5	   /*Оценка студента*/
+	)
+	{
+		std::array<Int, mcount> marks{Mark1, Mark2, Mark3, Mark4,
+					      Mark5};
+		add(Student(N, Surname, Name, Patronim, AvgMark_(marks),
+			    marks));
+		return *this;
+	}
+
+	/*Вывод без форматирования*/
+	friend std::ostream & operator<<(std::ostream & ost,
+					 StudentList const & list)
+	{
+		for (auto rec : list.list_) {
+			ost << rec.N << ' ' << rec.Surname << ' ' << rec.Name;
+			ost << ' ' << rec.Patronim << ' ' << rec.AvgMark << ' ';
+			for (auto mark : rec.Marks)
+				ost << mark << ' ';
+			ost << '\n';
+		}
+
+		return ost;
+	}
+
+private:
+	static constexpr auto mcount{5ul}; /*Число оценок*/
+
+	/*Запись студента*/
+	struct Student {
+		Int N;			       /*Номер студента*/
+		String Surname,		       /*Фамилия*/
+			Name,		       /*Имя*/
+			Patronim;	       /*Отчество*/
+		Double AvgMark;		       /*Средний бал*/
+		std::array<Int, mcount> Marks; /*Оценки студента*/
+
+		/*Конструктор записи*/
+		Student(Int N,
+			String Surname,
+			String Name,
+			String Patronim,
+			Double AvgMark,
+			std::array<Int, mcount> Marks)
+			: N{N}, Surname{Surname}, Name{Name},
+			  Patronim{Patronim}, AvgMark{AvgMark}, Marks{Marks}
+		{
+		}
+	};
+
+	/*Список студентов*/
+	std::vector<StudentList::Student> list_;
+
+	/*Добавление готовой записи со студентом*/
+	StudentList & add(Student const & A);
+
+	/*Подсчёт средней оценки*/
+	Double AvgMark_(std::array<Int, mcount> const & marks)
+	{
+		Int sum{};
+		for (auto mark : marks)
+			sum += mark;
+
+		return sum.get() / static_cast<double>(marks.size());
+	}
+
+	Int MaxSurnameWidth_{0},      /*Самая широкая фамилия*/
+		MaxNameWidth_{0},     /*Самое широкое имя*/
+		MaxPatronimWidth_{0}; /*Самое широкое отчество*/
+};
+
+StudentList & StudentList::add(Student const & A)
+{
+	MaxNameWidth_ = static_cast<int>(std::max(
+		static_cast<size_t>(MaxNameWidth_.get()), A.Name.length()));
+	MaxSurnameWidth_ = static_cast<int>(
+		std::max(static_cast<size_t>(MaxSurnameWidth_.get()),
+			 A.Surname.length()));
+	MaxPatronimWidth_ = static_cast<int>(
+		std::max(static_cast<size_t>(MaxPatronimWidth_.get()),
+			 A.Patronim.length()));
+
+	list_.push_back(A);
+	return *this;
+}
